e112ac8e604e532b4f7b0f10e15e27bd90f753f7
Behdad Esfahbod <behdad@behdad.org>
2013-01-07 21:30:22 -0600

    Relicense to Unicode-compatible license

--- pybyedie.py
+++ pybyedie.py
@@ -4,17 +4,34 @@
 #
 # PyByeDie, a reference implementation of UAX#9 Unicode Bidirectional Algorithm.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# the Unicode data files and any associated documentation (the "Data Files") or
+# Unicode software and any associated documentation (the "Software") to deal in
+# the Data Files or Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, and/or sell copies
+# of the Data Files or Software, and to permit persons to whom the Data Files or
+# Software are furnished to do so, provided that (a) the above copyright
+# notice(s) and this permission notice appear with all copies of the Data Files
+# or Software, (b) both the above copyright notice(s) and this permission notice
+# appear in associated documentation, and (c) there is clear notice in each
+# modified Data File or in the Software as well as in the documentation
+# associated with the Data File(s) or Software that the data or software has been
+# modified.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# THE DATA FILES AND SOFTWARE ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD
+# PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS INCLUDED IN
+# THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL
+# DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+# WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+# OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THE DATA FILES OR
+# SOFTWARE.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Except as contained in this notice, the name of a copyright holder shall not be
+# used in advertising or otherwise to promote the sale, use or other dealings in
+# these Data Files or Software without prior written authorization of the
+# copyright holder.
 #
 # Google Author(s):
 #   Behdad Esfahbod
